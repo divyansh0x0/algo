f9c319cd24a877a7cd43c2298a5f24e8b8ed91e7
divyansh0x0 <divyansh0x0@gmail.com>
2026-01-25 01:56:56 +0530

    Convert to resizable overlay view of editor over the simulation window.

--- app/components/DividerContainer.vue
+++ app/components/DividerContainer.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 const divider = ref<HTMLDivElement | null>(null);
-const left = ref<HTMLElement | null>(null);
 const right = ref<HTMLElement | null>(null);
 const container = ref<HTMLDivElement | null>(null);
 const props = defineProps({
@@ -9,72 +8,85 @@ const props = defineProps({
         default: 100,
     }
 });
-onMounted(() => {
-    if (!divider.value || !container.value) {
-        return;
-    }
+let dragging = false;
+let mouse_down_x = 0;
 
-    const divider_comp = divider.value;
-    let dragging = false;
-    let mouse_down_x = 0;
-    divider_comp.onmousedown = (e) => {
-        mouse_down_x = e.clientX;
-        dragging = true;
-        divider_comp.classList.add("active");
-    };
-    window.addEventListener("mouseup", () => {
-        dragging = false;
-        divider_comp.classList.remove("active");
-    });
-    window.addEventListener("mousemove", (e: MouseEvent) => {
-        if (dragging) {
-            if (!divider.value || !container.value) {
-                return;
-            }
-            const container_bounds = container.value.getBoundingClientRect();
-            const divider_bounds = divider_comp.getBoundingClientRect();
-            const x_pos_centered = e.clientX + (divider_bounds.left + divider_bounds.width / 2) - mouse_down_x;
-            const x_pos = Math.max(props.padding + container_bounds.left, Math.min(x_pos_centered, container_bounds.width - props.padding));
-            const left_flex_basis = x_pos / container_bounds.width * 100;
-            const right_flex_basis = 100 - left_flex_basis;
-            if (left.value && right.value) {
-                left.value.style.flex = `0 0 ${left_flex_basis}%`;
-                right.value.style.flex = `0 0 ${right_flex_basis}%`;
-            }
-            divider_comp.style.left = `${left.value?.clientWidth}px`;
-            mouse_down_x = e.clientX;
+function dragMove(x: number): void {
+    if (dragging) {
+        if (!divider.value || !container.value) {
+            return;
         }
+        const container_bounds = container.value.getBoundingClientRect();
+        const divider_bounds = divider.value.getBoundingClientRect();
+        const x_pos_centered = x + (divider_bounds.left + divider_bounds.width / 2) - mouse_down_x;
+        const x_pos = Math.max(props.padding + container_bounds.left, Math.min(x_pos_centered, container_bounds.width - props.padding));
+        const right_width = Math.max(window.innerWidth - x_pos, 0);
+        if (right.value) {
+            right.value.style.width = `${right_width}px`;
+        }
+        mouse_down_x = x;
+    }
+}
+
+function dragEnd(): void {
+    dragging = false;
+    divider.value?.classList.remove("active");
+}
+
+function dragStart(x: number): void {
+    mouse_down_x = x;
+    dragging = true;
+    divider.value?.classList.add("active");
+}
+
+onMounted(() => {
+    divider.value?.addEventListener("mousedown", (e) => {
+        dragStart(e.clientX);
     });
-    window.addEventListener("resize", () => {
-        divider_comp.style.left = `${left.value?.clientWidth}px`;
+    divider.value?.addEventListener("touchstart", (e) => {
+        const touch = e.touches[0];
+        if (touch)
+            dragStart(touch.clientX);
+    });
+    window.addEventListener("mouseup", () => {
+        dragEnd();
+    });
+
+    window.addEventListener("touchend", () => {
+        dragEnd();
+    });
+    window.addEventListener("mousemove", (e) => {
+        dragMove(e.clientX);
+    });
+
+    window.addEventListener("touchmove", (e) => {
+        const touch = e.touches[0];
+        if (touch)
+            dragMove(touch.clientX);
     });
 });
 </script>
 
 <template>
-    <div ref="container" class="container" v-bind="$attrs">
+    <div ref="container" v-bind="$attrs">
         <div ref="left" class="left">
             <slot name="left"/>
         </div>
-        <div ref="divider" class="slider" style="">
-            <Icon class="icon" name="material-symbols:drag-indicator" width="24"/>
-        </div>
         <div ref="right" class="right">
+            <div ref="divider" class="slider" style="">
+                <Icon class="icon" name="material-symbols:drag-indicator" width="24"/>
+            </div>
             <slot name="right"/>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-.container {
-    display: flex;
-    position: relative
-}
 .slider {
     z-index: 1;
     width: 30px;
     height: 100%;
     position: absolute;
-    left: 50%;
+    left: 0;
     transform: translateX(-50%);
     background: none;
     cursor: ew-resize;
@@ -107,9 +119,19 @@ onMounted(() => {
         z-index: 1;
     }
 }
-.right, .left{
-    flex: 1;
+
+.left {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+.right {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 50%;
+    height: 100%;
     background-color: var(--color-background);
-    overflow: auto;
 }
 </style>
